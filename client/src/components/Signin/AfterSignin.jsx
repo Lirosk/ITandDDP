89584310f6ea14e6585e2de61522efff519dba79
Lirosk <kirill_crysis@bk.ru>
2022-09-11 20:00:30 +0300

    Add getting user country.

--- client/src/components/Signin/AfterSignin.jsx
+++ client/src/components/Signin/AfterSignin.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import useDevice from '../../hooks/useDevice';
+import { getUserData } from '../../js/api';
 import Main from '../Main/Main';
 
 
 export default function AfterSignin() {
   const deviceId = useDevice();
+  getUserData();
 
   return (
     <Main />
